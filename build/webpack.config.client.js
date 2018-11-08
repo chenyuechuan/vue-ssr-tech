15ffe1e07bbff1149cd8a5ca7329ca6e6100490c
陈越川 <chenyuechuan_dz@sm.vvip-u.com>
2018-11-08 18:35:26 +0800

    'cyc'

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -5,6 +5,7 @@ const Webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
+const VueClientPlugin = require('vue-server-renderer/server-plugin')
 
 const devServer = {
     port: 8002,
@@ -27,7 +28,8 @@ const defaultPlugins = [
     }),
     new HTMLPlugin({
         template: path.join(__dirname, './template.html')
-    })
+    }),
+    new VueClientPlugin()
 ]
 
 let config
---
+++ build/webpack.config.server.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const Webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const VueServerPlugin = require('vue-server-renderer/server-plugin')
+
+let config
+
+config = merge(baseConfig, {
+    target: 'node',
+    entry: path.join(__dirname, '../client/server-entry.js'),
+    output: {
+        libraryTarget: 'commonjs2',
+        filename: 'server-entry.js',
+        path: path.join(__dirname, '../server-build')
+    },
+    enternals: Object.keys(require('../package.json').dependencies),
+    module: {
+        rules: [
+            {
+                test: /\.styl$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'vue-style-loader',
+                    use: [
+                        'css-loader',
+                        {loader: 'postcss-loader', options: {sourceMap: true}},
+                        'stylus-loader'
+                    ]
+                })
+            }
+        ]
+    },
+    devtool: 'source-map',
+    plugins: [
+        new ExtractTextPlugin('style.[contentHash:8].css'),
+        new Webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+            'process.env.VUE_ENV': '"server"'
+        }),
+        new VueServerPlugin()
+    ]
+})
+
+module.exports = config
